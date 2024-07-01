11506010293c5b06aa946db87055fc612512331f
Lara Ezarik <laraezarik@gmail.com>
2024-07-01 13:18:14 +0100

    feat: added file upload layout

--- frontend/src/views/UploadView.vue
+++ frontend/src/views/UploadView.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="about">
-    <h1>This is a upload page</h1>
+  <div class="container">
+    <div class="card mt-4">
+      <div class="card-body">
+        <h5 class="card-title">Upload a photo</h5>
+        <div class="input-group mb-3">
+          <input type="file" class="form-control" placeholder="" aria-label="Example text with button addon" aria-describedby="button-addon1">
+        </div>
+        <button class="btn btn-primary">Upload</button>
+      </div>
+    </div>
   </div>
 </template>
