3459156f11b6c9ab17519b7588f5312d796ce7a6
Lara Ezarik <laraezarik@gmail.com>
2024-07-01 13:35:21 +0100

    feat: added layout for home page

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -2,7 +2,25 @@
 </script>
 
 <template>
-  <main>
-    <h1>This is a home page</h1>
-  </main>
+  <div class="container">
+    <div class="row mt-4">
+      <div class="col=md-12">
+        <h1>Pick an image</h1>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-6">
+        <div class="card mt-4">
+          <div class="card-body">
+          </div>
+         </div>
+      </div>
+      <div class="col-md-6">
+        <div class="card mt-4">
+          <div class="card-body">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
