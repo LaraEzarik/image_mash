b34e17b98b872bb897f4d64359924dc29b34cc81
Lara Ezarik <laraezarik@gmail.com>
2024-07-01 13:48:15 +0100

    feat: results page base layout

--- frontend/src/views/ResultsView.vue
+++ frontend/src/views/ResultsView.vue
@@ -1,5 +1,29 @@
 <template>
-    <div class="about">
-      <h1>This is a results page</h1>
+    <div class="container">
+        <div class="row mt-4">
+            <div class="col=md-12">
+                <h1>Results</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card mt-4">
+                    <div class="card-body">
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card mt-4">
+                    <div class="card-body">
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card mt-4">
+                    <div class="card-body">
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
   </template>
