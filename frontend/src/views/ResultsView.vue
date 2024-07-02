ff49bef14e3f90f6b90e875e31775bd84f0d60c3
Lara Ezarik <laraezarik@gmail.com>
2024-07-02 20:35:18 +0100

    feat: added basic results page

--- frontend/src/views/ResultsView.vue
+++ frontend/src/views/ResultsView.vue
@@ -5,25 +5,34 @@
                 <h1>Results</h1>
             </div>
         </div>
-        <div class="row">
-            <div class="col-md-4">
-                <div class="card mt-4">
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+            <div class="col" v-for="image in result.getAllImages" :key="image.id">
+                <div class="card shadow-sm">
+                    <img :src="image.image" class="img-fluid" />
                     <div class="card-body">
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="card mt-4">
-                    <div class="card-body">
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="card mt-4">
-                    <div class="card-body">
+                        <p class="card-text">ELO:{{ image.elo }}</p>
                     </div>
                 </div>
             </div>
         </div>
     </div>
   </template>
+
+  <script setup>
+    import { useQuery } from '@vue/apollo-composable'
+    import { gql } from 'graphql-tag'
+
+    const GET_RESULTS = gql`
+      query getAllImages {
+          getAllImages {
+            id
+            image
+            upvotes
+            downvotes
+            elo
+        }
+      }
+    `;
+
+    const { result } = useQuery(GET_RESULTS)
+</script>
