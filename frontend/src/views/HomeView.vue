1eb6d5ac6483daf478bd1470f746cc23e0d0fafb
Lara Ezarik <laraezarik@gmail.com>
2024-07-02 20:16:17 +0100

    fix: fixed frontend for updated backend

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -3,8 +3,14 @@ import { useQuery } from '@vue/apollo-composable'
 import { gql } from 'graphql-tag'
 
 const { result } = useQuery(gql`
-      query randomImages {
-        randomImages
+      query compareImages {
+        compareImages {
+            id
+            image
+            upvotes
+            downvotes
+            elo
+          }
       }
     `);
 </script>
@@ -19,15 +25,45 @@ const { result } = useQuery(gql`
     <div class="row">
       <div class="col-md-6">
         <div class="card mt-4">
-          <div class="card-body">
-            <img :src="result.randomImages[0]" class="img-fluid" />
+          <div class="card-body text-center">
+            <div class="row">
+              <div class="col-md-12">
+                <img :src="result.compareImages[0].image" class="img-fluid" />
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-4">
+                <p>Upvotes: {{ result.compareImages[0].upvotes }}</p>
+              </div>
+              <div class="col-md-4">
+                <p>Downvotes: {{ result.compareImages[0].downvotes }}</p>
+              </div>
+              <div class="col-md-4">
+                <p>Elo: {{ result.compareImages[0].elo }}</p>
+              </div>
+            </div>
           </div>
          </div>
       </div>
       <div class="col-md-6">
         <div class="card mt-4">
-          <div class="card-body">
-            <img :src="result.randomImages[1]" class="img-fluid" />
+          <div class="card-body text-center">
+            <div class="row">
+              <div class="col-md-12">
+                <img :src="result.compareImages[1].image" class="img-fluid" />
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-4">
+                <p>Upvotes: {{ result.compareImages[1].upvotes }}</p>
+              </div>
+              <div class="col-md-4">
+                <p>Downvotes: {{ result.compareImages[1].downvotes }}</p>
+              </div>
+              <div class="col-md-4">
+                <p>Elo: {{ result.compareImages[1].elo }}</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
